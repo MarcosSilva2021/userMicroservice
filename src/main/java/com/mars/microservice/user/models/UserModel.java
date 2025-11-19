6985ac8efc708697ccd148fe9f7699c1b6879953
MarcosSilva2021 <mfsilva5585@gmail.com>
2025-11-18 21:57:14 -0300

    up model

--- src/main/java/com/mars/microservice/user/models/UserModel.java
+++ src/main/java/com/mars/microservice/user/models/UserModel.java
@@ -1,7 +1,6 @@
 package com.mars.microservice.user.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.UUID;
@@ -11,8 +10,33 @@ import java.util.UUID;
 public class UserModel implements Serializable {
     private static final long seriaVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
     private String name;
     private String email;
 
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
