78a0f80f2d48d9eb056ae377d4c33ccff63152d3
MarcosSilva2021 <mfsilva5585@gmail.com>
2025-11-20 22:07:47 -0300

    create userController

---
+++ src/main/java/com/mars/microservice/user/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.mars.microservice.user.controller;
+
+import com.mars.microservice.user.dtos.UserRecordDto;
+import com.mars.microservice.user.models.UserModel;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
+
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRecordDto, userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body();
+
+    }
+}
