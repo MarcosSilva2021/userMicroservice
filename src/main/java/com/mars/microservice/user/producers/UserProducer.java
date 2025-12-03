c6531c3008b040007ec376d263f459dda3f8f73c
MarcosSilva2021 <mfsilva5585@gmail.com>
2025-12-02 21:41:51 -0300

    create Producers

---
+++ src/main/java/com/mars/microservice/user/producers/UserProducer.java
@@ -0,0 +1,19 @@
+package com.mars.microservice.user.producers;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
+
+}
