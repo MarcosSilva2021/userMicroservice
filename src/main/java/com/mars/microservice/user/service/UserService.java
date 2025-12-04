ac4f119e74dd2be516a43e70196499b7ba51bd27
MarcosSilva2021 <mfsilva5585@gmail.com>
2025-12-03 21:42:12 -0300

    update method save - add PublishMsg

--- src/main/java/com/mars/microservice/user/service/UserService.java
+++ src/main/java/com/mars/microservice/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.mars.microservice.user.service;
 
 import com.mars.microservice.user.models.UserModel;
+import com.mars.microservice.user.producers.UserProducer;
 import com.mars.microservice.user.repositories.UserRepository;
 
 import org.springframework.stereotype.Service;
@@ -10,13 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
